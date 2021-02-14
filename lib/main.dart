0a0c9c6793f0f3cd5418e02dc34b92e885c5f59c
kyorohiro <kyorohiro@gmail.com>
2021-02-15 06:05:56 +0900

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,41 @@
 import 'package:flutter/material.dart';
 
+// https://flutter.dev/docs/cookbook/animation/physics-simulation
+
 void main() {
-  runApp(MaterialApp(title: "Flutter layout", home: Page1()));
+  runApp(MaterialApp(title: "Flutter layout", home: PhysicsCardsDragDemo()));
 }
 
-class Page1 extends StatelessWidget {
+class PhysicsCardsDragDemo extends StatelessWidget {
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Go"),
-          onPressed: () {
-            print("on ress");
-            Navigator.of(context).push(_createRoute());
-          },
+      body: DraggableCard(
+        child: FlutterLogo(
+          size: 128,
         ),
       ),
     );
   }
-
-  Route _createRoute() {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) {
-        return Page2();
-      },
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.5, 0.5);
-        var end = Offset(0.0, 0.0);
-       // var tween = Tween(begin: begin, end: end);
-        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.ease));
-
-        //var offsetAnimation = animation.drive(tween);
-        //return SlideTransition(position: offsetAnimation, child: child,);
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-/*
-      var curveAnimation = CurvedAnimation(parent: animation, curve: Curves.ease);
-      return SlideTransition(
-        position: tween.animate(curveAnimation), //
-        child: child//
-        );
-      */
-      },
-    );
-  }
 }
 
-class Page2 extends StatelessWidget {
+class DraggableCard extends StatefulWidget {
+  final Widget child;
+  DraggableCard({this.child});
+  @override
+  State<StatefulWidget> createState() {
+    return DraggableCardState();
+  }
+  
+}
+
+class DraggableCardState extends State<DraggableCard> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Page 2'),
-      ),
+    return Align(
+      child:  Card(child:  widget.child),
     );
   }
+  
 }
